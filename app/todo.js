f40f3ab4db0929bc48d5b28bc37d89bf4ea066e3
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-11-19 23:57:15 +0900

    Fix js code for PR #1

--- app/todo.js
+++ app/todo.js
@@ -1,120 +1,200 @@
-// 編集ボタン作成
-function createEditButton() {
+let id = 0; // タスク管理用ID(初期値0)
+let tasksStore = []; // 各タスク格納用配列
+
+const inputTaskContent = document.getElementById("inputTaskContent");
+const saveButton = document.getElementById("saveButton");
+const taskList = document.getElementById("taskList");
+
+// タスク保存
+saveButton.addEventListener("click", () => {
+  createTask();
+});
+
+// タスク単体の生成
+const createTask = () => {
+  if (!inputTaskContent.value) {
+    alert("タスク内容を入力してください");
+  } else {
+    tasksStore.push(
+      // タスクID、タスク内容、タスクステータス（デフォルトは未完了）を格納
+      { id, taskValue: inputTaskContent.value, isChecked: false }
+    );
+    renderTask(id, inputTaskContent.value);
+    id++;
+    inputTaskContent.value = "";
+
+    taskStatuses[0].count++; // 全タスクに加算
+    taskStatuses[2].count++; // 未完了タスクに加算
+
+    updateTaskList();
+  }
+};
+
+// タスクステータス管理用配列
+const taskStatuses = [
+  { label: "全タスク", count: 0 },
+  { label: "済", count: 0 },
+  { label: "未完了", count: 0 },
+];
+
+// タスク一覧の更新
+const updateTaskList = () => {
+  const taskStatusPanel = document.getElementById("taskStatusPanel");
+
+  // タスク追加毎にステータスパネルが増えないようにする
+  while (taskStatusPanel.firstChild) {
+    taskStatusPanel.removeChild(taskStatusPanel.firstChild);
+  }
+
+  taskStatuses.forEach((taskStatus) => {
+    const taskState = document.createElement("li");
+
+    // 各ステータス情報をそれぞれ上書き
+    taskState.innerText = `${taskStatus.label} \n ${taskStatus.count}`;
+    // タスクステータスを反映
+    taskStatusPanel.appendChild(taskState);
+  });
+};
+
+const confirmCheckBox = (id) => {
+  const currentTaskContent = tasksStore.find((targetTask) => {
+    return targetTask.id === id;
+  });
+
+  currentTaskContent.isChecked = !currentTaskContent.isChecked;
+
+  // チェックボックスのチェック有無判定
+  if (currentTaskContent.isChecked) {
+    taskStatuses[1].count++; // 完了タスクに加算
+    taskStatuses[2].count--; // 未完了タスクから減算
+    // !currentTaskContent.isCheckedの場合
+  } else {
+    taskStatuses[1].count--; // 未完了タスクに加算
+    taskStatuses[2].count++; // 完了タスクから減算
+  }
+
+  // タスク数を反映
+  updateTaskList();
+};
+
+// タスクの描画
+const renderTask = (id, taskValue) => {
+  const eachTask = document.createElement("div");
+  eachTask.setAttribute("id", `eachTask${id}`); // タスク追加時にIDをふる
+  eachTask.classList.add("each-task");
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.setAttribute("onclick", `confirmCheckBox(${id})`);
+
+  const taskContent = document.createElement("li");
+  taskContent.innerHTML = taskValue;
+  taskContent.id = "taskContent";
+
   const editButton = document.createElement("button");
   editButton.type = "button";
   editButton.innerText = "編集";
-  editButton.setAttribute("onclick", "editTask(event)");
-  editButton.setAttribute("id", "editButton");
-  return editButton;
-}
+  editButton.setAttribute("onclick", `updateTask(${id})`);
+  editButton.id = "editButton";
 
-// タスク単体の生成
-function createTask(inputTaskValue, eachTask) {
-  const checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  checkBox.setAttribute("onclick", "taskStatusCheck()");
-
-  // タスク内容をチェックボックスボックスの値（label）とする
-  const taskContent = document.createElement("label");
-  taskContent.appendChild(document.createTextNode(inputTaskValue));
-
-  const editButton = createEditButton();
-
-  const deleteButton = createEditButton(); // 編集ボタンを削除ボタンに上書き生成
+  const deleteButton = document.createElement("button");
+  deleteButton.type = "button";
   deleteButton.innerText = "削除";
-  deleteButton.setAttribute("onclick", "deleteTask(event)");
-  deleteButton.setAttribute("id", "deleteButton");
+  deleteButton.setAttribute("onclick", `deleteTask(${id})`);
+  deleteButton.id = "deleteButton";
 
   eachTask.appendChild(checkBox);
   eachTask.appendChild(taskContent);
   eachTask.appendChild(editButton);
   eachTask.appendChild(deleteButton);
-}
-
-// タスク保存
-function saveTask(taskValue) {
-  const inputTaskValue = document.getElementById(taskValue).value;
-  if (!inputTaskValue) {
-    // タスク未入力の場合は保存せずアラートを出す
-    alert("タスク内容を入力してください");
-  } else {
-    const parentTaskList = document.getElementById("taskList");
-    const eachTask = document.createElement("form");
-    eachTask.setAttribute("id", "eachTask");
-    createTask(inputTaskValue, eachTask);
-    parentTaskList.appendChild(eachTask);
-    taskStatusCheck();
-    // タスク保存後、入力欄をクリアにする
-    document.getElementById("inputTaskForm").reset();
-  }
-}
+  taskList.appendChild(eachTask);
+};
 
 // タスク編集
-function editTask(event) {
-  // クリックイベント時にタスク親要素を取得
-  const eachTaskElement = event.target.parentElement;
-  const editValue = eachTaskElement.childNodes[1].textContent;
-  const inputEditValue = document.createElement("input");
+const updateTask = (id) => {
+  // 編集対象タスクidと一致するタスクのみを取得
+  const currentTaskContent = tasksStore.find((targetTask) => {
+    return targetTask.id === id;
+  });
 
-  // タスク表示部分の要素を編集可能にする
-  inputEditValue.setAttribute("type", "text");
-  inputEditValue.setAttribute("id", "editTask");
-  inputEditValue.value = editValue;
+  eachTaskElement = document.getElementById(`eachTask${id}`);
 
-  // 編集中は元のタスク内容を削除する
-  while (eachTaskElement.hasChildNodes()) {
-    eachTaskElement.removeChild(eachTaskElement.firstChild);
-  }
-  const editSaveButton = createEditButton();
+  const editTaskForm = document.createElement("input");
+  editTaskForm.setAttribute("id", "editTaskForm");
+
+  const editTargetTask = eachTaskElement.children[1];
+
+  editButton = eachTaskElement.children[2];
+  editButton.style.display = "none";
+  deleteButton = eachTaskElement.children[3];
+  deleteButton.style.display = "none";
+
+  const backButton = document.createElement("button");
+  backButton.type = "button";
+  backButton.innerHTML = "戻る";
+  backButton.id = "backButton";
+
+  const editSaveButton = document.createElement("button");
+  editSaveButton.type = "button";
   editSaveButton.innerText = "保存";
+  editSaveButton.id = "editSaveButton";
 
-  // 保存ボタンクリックで、編集タスク内容を保存
-  editSaveButton.setAttribute("onclick", "inputEditedTaskValue(event)");
-
-  eachTaskElement.appendChild(inputEditValue);
+  eachTaskElement.replaceChild(editTaskForm, editTargetTask);
+  eachTaskElement.appendChild(backButton);
   eachTaskElement.appendChild(editSaveButton);
-}
 
-function inputEditedTaskValue(event) {
-  const inputEditedValue = document.getElementById("editTask").value;
-  if (!inputEditedValue) {
-    // タスク未入力の場合は保存せずアラートを出す
-    alert("編集中のタスク内容を入力してください");
-  } else {
-    const eachTaskElement = event.target.parentElement;
+  // 編集済タスク保存
+  editSaveButton.addEventListener("click", () => {
+    if (!editTaskForm.value) {
+      alert("タスク内容を入力してください");
+    } else {
+      currentTaskContent.value = editTaskForm.value;
+      const editedTask = document.createElement("li");
+      editedTask.innerHTML = currentTaskContent.value;
+      editedTask.setAttribute("id", "taskContent");
 
-    while (eachTaskElement.hasChildNodes()) {
-      eachTaskElement.removeChild(eachTaskElement.firstChild);
+      eachTaskElement.replaceChild(editedTask, editTaskForm);
+      eachTaskElement.removeChild(editSaveButton);
+      eachTaskElement.removeChild(backButton);
+      editButton.style.display = "block";
+      deleteButton.style.display = "block";
     }
-    createTask(inputEditedValue, eachTaskElement);
-    taskStatusCheck();
-  }
-}
+  });
 
-function deleteTask(event) {
+  // 編集中タスク内容を破棄して戻る
+  backButton.addEventListener("click", () => {
+    const currentTaskContent = tasksStore.find((targetTask) => {
+      return targetTask.id === id;
+    });
+
+    const originalTask = document.createElement("li");
+    originalTask.innerHTML = currentTaskContent.taskValue;
+    originalTask.id = "taskContent";
+
+    eachTaskElement.replaceChild(originalTask, editTaskForm);
+    eachTaskElement.removeChild(editSaveButton);
+    eachTaskElement.removeChild(backButton);
+    editButton.style.display = "block";
+    deleteButton.style.display = "block";
+  });
+};
+
+// タスク削除
+const deleteTask = (id) => {
   if (confirm("本当によろしいですか？")) {
-    const eachTaskElement = event.target.parentElement;
-    eachTaskElement.remove();
-    taskStatusCheck();
-  } else {
-    return;
+    const currentTaskContent = tasksStore.find((targetTask) => {
+      return targetTask.id === id;
+    });
+
+    const eachTask = document.getElementById(`eachTask${id}`);
+    eachTask.remove();
+
+    taskStatuses[0].count--; // 全タスクから減算
+    if (currentTaskContent.isChecked) {
+      taskStatuses[1].count--; // 完了タスクから減算
+    } else {
+      taskStatuses[2].count--; // 未完了タスクから減算
+    }
+    updateTaskList();
   }
-}
-
-function taskStatusCheck() {
-  // タスク数を取得
-  let countAll = document.getElementById("countAll");
-  let countDone = document.getElementById("countDone");
-  let countNotYet = document.getElementById("countNotYet");
-
-  // チェックボックスのチェック有無判定
-  const checkedTask = document.querySelectorAll("input[type=checkbox]:checked");
-  const unCheckedTask = document.querySelectorAll(
-    "input[type=checkbox]:not(:checked)"
-  );
-
-  // タスク数を反映
-  countAll.innerHTML = checkedTask.length + unCheckedTask.length;
-  countDone.innerHTML = checkedTask.length;
-  countNotYet.innerHTML = unCheckedTask.length;
-}
+};
