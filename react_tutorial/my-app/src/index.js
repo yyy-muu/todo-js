0411ccd012b5e620cf3fb1fd03fec766fc2befd3
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-06 16:38:14 +0900

    Add highlight feature for win

--- react_tutorial/my-app/src/index.js
+++ react_tutorial/my-app/src/index.js
@@ -5,7 +5,10 @@ import "./index.css";
 // 子
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button
+      className={props.highlight ? `square win-line` : `square`}
+      onClick={props.onClick}
+    >
       {props.value}
     </button>
   );
@@ -20,6 +23,7 @@ class Board extends React.Component {
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)} //マス目クリックで、Square側で関数呼び出しさせる
         key={i} // マスをループにし、キーを持たせる
+        highlight={this.props.winLine.includes(i)}
       />
     );
   }
@@ -99,7 +103,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winInfo = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       // console.log(step);
@@ -118,8 +122,10 @@ class Game extends React.Component {
     });
 
     let status;
-    if (winner) {
-      status = "Winner: " + winner;
+    let winLine = [];
+    if (winInfo) {
+      status = "Winner: " + winInfo.player;
+      winLine = winInfo.line;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -132,6 +138,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winLine={winLine}
           />
         </div>
         <div className="game-info">
@@ -163,7 +170,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        player: squares[a],
+        line: [a, b, c],
+      };
     }
   }
   return null;
